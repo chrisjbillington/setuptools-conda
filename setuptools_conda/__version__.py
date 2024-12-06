11e9039fc925ea8be7b72d647ad0e50743680c2d
chrisjbillington <chrisjbillington@gmail.com>
2024-12-06 15:55:42 +1100

    use version-scheme=release-breanch-semver for consistency with other projects even though we don't use release branches

--- setuptools_conda/__version__.py
+++ setuptools_conda/__version__.py
@@ -4,7 +4,9 @@ import importlib.metadata
 root = Path(__file__).parent.parent
 if (root / '.git').is_dir():
     from setuptools_scm import get_version
-    __version__ = get_version(root, local_scheme="no-local-version")
+    __version__ = get_version(
+        root, version_scheme="release-branch-semver", local_scheme="no-local-version"
+    )
 else:
     try:
         __version__ = importlib.metadata.version(__package__)
